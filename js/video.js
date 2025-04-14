47969df423596f90f70cbd8501ce4c66d99fdcab
hiraeiraj <daud@umich.edu>
2025-04-14 16:23:29 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -89,6 +89,7 @@ window.addEventListener("DOMContentLoaded", function(){
 window.addEventListener("DOMContentLoaded", function(){
 	mute=this.document.querySelector("#mute")
 	video=this.document.querySelector("#player1")
+	volume=this.document.querySelector("#volume")
 
 	mute.addEventListener("click", function() {
 		video.muted = !video.muted
@@ -98,7 +99,7 @@ window.addEventListener("DOMContentLoaded", function(){
 			volume.textContent = "Muted";
 			mute.textContent = "Unmute"
 		} else {
-			volume.textContent = video.volume*100 +"%";
+			volume.textContent = video.volume * 100 + "%";
 			mute.textContent = "Mute"
 		}
 	})
@@ -109,13 +110,14 @@ window.addEventListener("DOMContentLoaded", function(){
 window.addEventListener("DOMContentLoaded", function(){
 	slider=this.document.querySelector("#slider")
 	video=this.document.querySelector("#player1")
+	volume=this.document.querySelector("#volume")
 
 	slider.addEventListener("input", function() {
 		video.volume = slider.value / 100
 		if (video.muted) {
 			volume.textContent = "Muted";
 		} else {
-			volume.textContent = video.volume*100 +"%";
+			volume.textContent = video.volume * 100 + "%";
 		}
 	})
 })
